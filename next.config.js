4cbe5a3745058a43bfda9a73b737ac39c6bba495
afganirw07 <afganirwansyah070708@gmail.com>
2025-08-27 22:13:06 +0700

    chore: delete   output: 'export'

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@
 //   '@fullcalendar/react',
 
 const nextConfig = {
-  output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
