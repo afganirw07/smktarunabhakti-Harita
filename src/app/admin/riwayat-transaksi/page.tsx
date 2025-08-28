76bfa4119ec003afdcedd88af6ab25cb855e53d7
afganirw07 <afganirwansyah070708@gmail.com>
2025-08-28 08:58:35 +0700

    feat : add button pdf

--- src/app/admin/riwayat-transaksi/page.tsx
+++ src/app/admin/riwayat-transaksi/page.tsx
@@ -4,8 +4,9 @@ import Riwayat from 'components/admin/riwayat-transaksi/riwayat'
 const RiwayatTransaksi = () => {
 
     return (
-        <div>
+        <div className='flex flex-col items-end p-4'>
             <Riwayat />
+            <button className='mt-10 bg-[#294B29] hover:bg-green-800 active:bg-[#294B29] text-white font-bold py-2 px-4 rounded'>Download Data</button>
         </div>
     )
 
