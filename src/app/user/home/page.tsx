c1fbb23868ce63844d3db98950b644cd4e5d3102
AhsanRosikhanYusri <ahsanbelajar2020@gmail.com>
2025-08-29 17:45:40 +0700

    style: home

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -254,7 +254,7 @@ const fetchData = async () => {
                 endDate={subscriptionEndDate}
               />
             </div>
-            <div className="mt-2 flex justify-between border-t pt-4 font-nunito text-sm">
+            <div className="w-full mt-2 flex nd:justify-between justify-center flex-wrap md:gap-0 gap-4 border-t pt-4 font-nunito text-sm">
               <div>
                 <p className="font-semibold text-green-500">Berlangganan</p>
                 <p className="font-bold text-black">
